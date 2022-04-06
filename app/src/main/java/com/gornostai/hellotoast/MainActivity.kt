99d52932eda2f1cb359bdc08cbe0e0d03be3d137
evg-gornostai <harnastai750@gmail.com>
2022-04-06 10:32:37 +0300

    Added increasing feature

--- app/src/main/java/com/gornostai/hellotoast/MainActivity.kt
+++ app/src/main/java/com/gornostai/hellotoast/MainActivity.kt
@@ -3,24 +3,41 @@ package com.gornostai.hellotoast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnToast: Button
+    private lateinit var btnCount: Button
+    private lateinit var tvShowCount: TextView
+
+    private var mCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvShowCount = findViewById(R.id.show_count)
+
         btnToast = findViewById(R.id.button_toast)
         btnToast.setOnClickListener {
             showToast()
+        }
+
+        btnCount = findViewById(R.id.button_count)
+        btnCount.setOnClickListener {
+            countUp()
         }
     }
 
     fun showToast(){
         Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show()
+    }
+
+    fun countUp(){
+        mCount++
+        tvShowCount.text = mCount.toString()
     }
 
 }
