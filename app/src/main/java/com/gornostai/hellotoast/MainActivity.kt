49749a93fbeda89d68e5ad453b75ab3205220eaa
evg-gornostai <harnastai750@gmail.com>
2022-04-06 10:22:43 +0300

    Added toast

--- app/src/main/java/com/gornostai/hellotoast/MainActivity.kt
+++ app/src/main/java/com/gornostai/hellotoast/MainActivity.kt
@@ -2,10 +2,25 @@ package com.gornostai.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnToast: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnToast = findViewById(R.id.button_toast)
+        btnToast.setOnClickListener {
+            showToast()
+        }
     }
+
+    fun showToast(){
+        Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show()
+    }
+
 }
